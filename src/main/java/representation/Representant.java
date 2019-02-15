78e45146ce23c381a424fca03ce190e7c0349512
AGUIRI <rayyane.aguiri@etud.univ-jfc.fr>
2019-02-15 11:38:05 +0100

    TPTHREAD

--- src/main/java/representation/Representant.java
+++ src/main/java/representation/Representant.java
@@ -11,11 +11,16 @@ public class Representant {
 	private String adresse;
 
 	private float salaireFixe;
+        
+        private ZoneGeographique secteur;
+        
+        private float[] listeCA= new float[12];
 
-	public Representant(int numero, String nom, String prenom, ZoneGeographique secteur) {
+	public Representant(int numero, String nom, String prenom,   ZoneGeographique secteur) {
 		this.numero = numero;
 		this.nom = nom;
 		this.prenom = prenom;
+                this .secteur= secteur;
 	}
 
 	public int getNumero() {
@@ -47,30 +52,35 @@ public class Representant {
 	}
 
 	public ZoneGeographique getSecteur() {
-		// TODO: Implémenter cette méthode
-		throw new UnsupportedOperationException("Pas encore implémenté");
+                return secteur ;
 	}
 
 	public void setSecteur(ZoneGeographique secteur) {
-		// TODO: Implémenter cette méthode
-		throw new UnsupportedOperationException("Pas encore implémenté");
+            this.secteur = secteur;
 	}
 
 	public void enregistrerCA(int mois, float montant) {
-		// vérifier les paramètres
 		if (mois < 0 || mois > 11) {
 			throw new IllegalArgumentException("Le mois doit être compris entre 0 et 11");
 		}
 		if (montant < 0) {
-			throw new IllegalArgumentException("Le montant doit être positif ou null");
+			throw new IllegalArgumentException("Le montant doit être superieur à 0") ;
 		}
-		// TODO: Implémenter cette méthode
-		throw new UnsupportedOperationException("Pas encore implémenté");
+                
+                listeCA[mois]=montant;
 	}
+        /** calcul le salaire mensuel de ce representant por un mois donné
+        *
+         * @param mois le numero de mois (de 0 à11)
+         * @param pourcentage le pourcentage à appliquer sur le CA réalisé par ce representant 
+         * @return  le salaire pour ce mois tenant compte du salire fixe et de l'indemnité repas 
+         */
 
 	public float salaireMensuel(int mois, float pourcentage) {
-		// TODO: Implémenter cette méthode
-		throw new UnsupportedOperationException("Pas encore implémenté");
+                if (mois < 0 || mois > 11) {
+                   throw new IllegalArgumentException("Le mois doit être compris entre 0 et 11");
+                }
+                return salaireFixe + pourcentage* listeCA[mois]+ secteur.getIndemniteRepas()  ;
 	}
 
 	@Override
